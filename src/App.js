9cc4cc4d4e172aab427f2b345cc72814692e4b14
David Kaufmann <david@kaufmann.dev>
2022-05-25 10:21:44 +0100

    feat(counter): implement counter with increment, decrement, hold, and reset

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const timer = useRef(null);
+
+  const increasehold = () => {
+    timer.current = setInterval(() => setCounter(count => count + 1), 250);
+  };
+
+  const decreasehold = () => {
+    timer.current = setInterval(() => setCounter(count => count - 1), 250);
+  };
+
+  function timeoutClear() {
+    clearInterval(timer.current);
+  }
+ 
+  const increase = () => {
+    setCounter(count => count + 1);
+  };
+ 
+  const decrease = () => {
+    setCounter(count => count - 1);
+  };
+ 
+  const reset = () =>{
+    setCounter(0)
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+      <h1 className="display-1 my-4">Counter</h1>
+      <button
+        className="btn btn-danger"
+        onClick={decrease}
+        onMouseLeave={timeoutClear}
+        onMouseUp={timeoutClear}
+        onMouseDown={decreasehold}
+      >-</button>
+      <span className="mx-4">{counter}</span>
+      <button
+        className="btn btn-primary"
+        onClick={increase}
+        onMouseLeave={timeoutClear}
+        onMouseUp={timeoutClear}
+        onMouseDown={increasehold}
+      >+</button><br></br>
+      <button className="btn btn-secondary d-inline-block mt-3" onClick={reset}>Reset</button>
     </div>
   );
 }
